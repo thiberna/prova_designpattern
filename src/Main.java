269706a1ea6ade8f5b1c21a5d6fa751397a2cae3
Thiago Bernardino <89875413+thiberna@users.noreply.github.com>
2024-11-19 20:19:22 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
             GlobalConfig config = GlobalConfig.getInstance();
 
             config.setGlobalDesignPattern("eu");
-            config.setDesignPattern2("eudenovo");
-            config.setDesignPattern3("eudenovoagain");
+            config.setDesignPattern2("eunocelular");
+            config.setDesignPattern3("eunopc");
 
             System.out.println(config.getGlobalDesignPattern());
             System.out.println(config.getDesignPattern2());
